fb4c7ee0f96dda59bb87b63201a8515e9c5c4a4a
Rishi-pro-code <rr9797@srmist.edu.in>
2026-02-27 11:15:47 +0530

    UC3:Hardcoded palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,4 +1,4 @@
-import java.util.*;
+
 class UseCase1PalindromeCheckerApp{
     void display_welcome(){
         System.out.println("Welcome to the Palindrome Checker Management System");
@@ -20,11 +20,28 @@ class UseCase2PalindromeCheckerApp{
         System.out.println(ch);
     }
 }
+class UseCase3PalindromeCheckerApp{
+    void uc3check(String word){
+        String rev="";
+        boolean c3=false;
+        for(int i=word.length()-1;i>=0;i--){
+            rev=rev+word.charAt(i);
+        }
+        if(word==rev){
+            c3=true;
+        }
+        System.out.println(c3);
+    }
+}
 public class PalindromeCheckerApp {
     public static void main(String[]args){
         UseCase1PalindromeCheckerApp welcome=new UseCase1PalindromeCheckerApp();
         welcome.display_welcome();
         UseCase2PalindromeCheckerApp uc2=new UseCase2PalindromeCheckerApp();
         uc2.uc2check();
+        UseCase3PalindromeCheckerApp uc3=new UseCase3PalindromeCheckerApp();
+        String wrd="";
+        uc3.uc3check(wrd);
     }
 }
+
