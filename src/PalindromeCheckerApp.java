770b2ac26d561a683d00e112181b2e82e7e2665b
Rishi-pro-code <rr9797@srmist.edu.in>
2026-02-27 11:22:03 +0530

    UC4: Hardcoded palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,4 +1,7 @@
 
+import java.util.*;
+
+
 class UseCase1PalindromeCheckerApp{
     void display_welcome(){
         System.out.println("Welcome to the Palindrome Checker Management System");
@@ -33,6 +36,89 @@ class UseCase3PalindromeCheckerApp{
         System.out.println(c3);
     }
 }
+class UseCase4PalindromeCheckerApp{
+    void uc4check(String wor){
+        char[] chars=wor.toCharArray();
+        int start=0;
+        int end=chars.length-1;
+        boolean ispalindrome=false;
+        while(start<end){
+            if(chars[start]==chars[end]){
+                ispalindrome=true;
+                start++;
+                end--;
+            }
+        }
+        System.out.println(ispalindrome);
+    }
+}
+class UseCase5PalindromeCheckerApp {
+
+    Stack<Character> stack = new Stack<>();
+
+    void uc5check(String word) {
+        char[] ch = word.toCharArray();
+        boolean ispalindrome = false;
+        for (char c : word.toCharArray()) {
+            stack.push(c);
+        }
+        for (int i = 0; i < word.length(); i++) {
+            if (stack.pop() == ch[i]) {
+                ispalindrome = true;
+            }
+        }
+        System.out.println(ispalindrome);
+
+    }
+}
+class UseCase6PalindromeCheckerApp {
+
+    void uc6check(String input) {
+
+        Queue<Character> queue = new LinkedList<>();
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : input.toCharArray()) {
+            queue.add(c);
+            stack.push(c);
+        }
+
+        boolean isPalindrome = true;
+
+        while (!queue.isEmpty()) {
+            if (!queue.remove().equals(stack.pop())) {
+                isPalindrome = false;
+                break;
+            }
+        }
+        System.out.println(isPalindrome);
+
+    }
+}
+class UseCase7PalindromeCheckerApp {
+
+    void uc7check(String input) {
+
+        Deque<Character> deque = new ArrayDeque<>();
+
+        for (char c : input.toCharArray()) {
+            deque.addLast(c);
+        }
+
+        boolean isPalindrome = true;
+
+        while (deque.size() > 1) {
+            if (!deque.removeFirst().equals(deque.removeLast())) {
+                isPalindrome = false;
+                break;
+            }
+        }
+        System.out.println(isPalindrome);
+
+    }
+}
+
+
 public class PalindromeCheckerApp {
     public static void main(String[]args){
         UseCase1PalindromeCheckerApp welcome=new UseCase1PalindromeCheckerApp();
@@ -42,6 +128,14 @@ public class PalindromeCheckerApp {
         UseCase3PalindromeCheckerApp uc3=new UseCase3PalindromeCheckerApp();
         String wrd="";
         uc3.uc3check(wrd);
+        UseCase4PalindromeCheckerApp uc4=new UseCase4PalindromeCheckerApp();
+        uc4.uc4check(wrd);
+        UseCase5PalindromeCheckerApp uc5 = new UseCase5PalindromeCheckerApp();
+        uc5.uc5check(wrd);
+        UseCase6PalindromeCheckerApp uc6 = new UseCase6PalindromeCheckerApp();
+        uc6.uc6check(wrd);
+        UseCase7PalindromeCheckerApp uc7 = new UseCase7PalindromeCheckerApp();
+        uc7.uc7check(wrd);
+
     }
 }
-
