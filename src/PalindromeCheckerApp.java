8799e1d3e1ffe715e87e5ce74f137ff78f45412e
Rishi-pro-code <rr9797@srmist.edu.in>
2026-03-09 09:32:42 +0530

    UC8:Linked List Based Palindrome Checker

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,15 +1,27 @@
-
-class UseCase1PalindromeCheckerApp{
-    void display_welcome(){
-        System.out.println("Welcome to the Palindrome Checker Management System");
-
+import java.util.*;
+class UseCase8PalindromeCheckerApp{
+    void uc8check(String input){
+        LinkedList<Character> list=new LinkedList<>();
+        for(char c:input.toCharArray()){
+            list.add(c);
+        }
+        boolean isPalindrome=false;
+        while(list.size()>1){
+            if(list.getFirst()==list.getLast()){
+                isPalindrome=true;
+                break;
+            }
+        }
+        System.out.println(isPalindrome);
     }
 }
-
-public class PalindromeCheckerApp {
+public class PalindromeCheckerApp{
     public static void main(String[]args){
-        UseCase1PalindromeCheckerApp welcome=new UseCase1PalindromeCheckerApp();
-        welcome.display_welcome();
+        String wrd="madam" ;
+        UseCase8PalindromeCheckerApp uc8=new UseCase8PalindromeCheckerApp();
+        uc8.uc8check(wrd);
+
 
     }
 }
+
