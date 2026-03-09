0d143f997c902cf2c72e6806ceb9ebfe56a5ede1
Rishi-pro-code <rr9797@srmist.edu.in>
2026-03-09 10:13:50 +0530

    UC9:Recursive Palindrome Checker

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,15 +1,24 @@
+class UseCase9PalindromeCheckerApp{
+    boolean uc9check(String input,int start,int end){
+        boolean isPalindrome=false;
+        while(start<end) {
+            if (input.charAt(start) == input.charAt(end)) {
+                isPalindrome = true;
 
-class UseCase1PalindromeCheckerApp{
-    void display_welcome(){
-        System.out.println("Welcome to the Palindrome Checker Management System");
+                start = start + 1;
+                end = end - 1;
+                uc9check(input, start, end);
 
+            }
+        }
+        return isPalindrome;
     }
 }
+public class PalindromeCheckerApp{
+    public static void main(String[] args) {
+        String wrd = "madam";
 
-public class PalindromeCheckerApp {
-    public static void main(String[]args){
-        UseCase1PalindromeCheckerApp welcome=new UseCase1PalindromeCheckerApp();
-        welcome.display_welcome();
-
+        UseCase9PalindromeCheckerApp uc9 = new UseCase9PalindromeCheckerApp();
+        System.out.println("Palindrome checker by UC9: " + uc9.uc9check(wrd, 0, wrd.length() - 1));
     }
 }
